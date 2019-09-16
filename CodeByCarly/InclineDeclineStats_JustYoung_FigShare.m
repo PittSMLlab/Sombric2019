d4757ed75da5cfc6a220fd030e9a95fb138bad8b
csombric <csombric@u.rochester.edu>
2019-09-16 15:51:31 -0400

    Visualization updates

--- CodeByCarly/InclineDeclineStats_JustYoung_FigShare.m
+++ CodeByCarly/InclineDeclineStats_JustYoung_FigShare.m
@@ -1,58 +1,50 @@
 %Stats for the incline decline study
-
-%% Young Figure 2, 3, 5
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Note that you need to change your current directory to have the data
+% files and this code present.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Young Figures
 clear all
 close all
 clc
-load('YoungSlopeData.mat')
-adaptDataList = {YoungSlopeData.DeclineYoungAbrupt.adaptData,YoungSlopeData.FlatYoungAbrupt.adaptData,YoungSlopeData.InclineYoungAbrupt.adaptData};
+load('StudyData.mat')
+adaptDataList = {StudyData.DeclineYoungAbrupt.adaptData,StudyData.FlatYoungAbrupt.adaptData,StudyData.InclineYoungAbrupt.adaptData};
 binWidth = 5;
 trialMarkerFlag = [0  0  0];
 indivSubFlag = 0;
 IndivSubList = 0;
 groups = {'DeclineYoungAbrupt','FlatYoungAbrupt','InclineYoungAbrupt'};
 
-%Figure 5: Kinetics
+% Step Length Visualization -- Figure 4 in the final paper
+StepLengthVis(StudyData, groups)
+
+%Figure 5 in the final paper
 params = {'FyBSmax','FyBFmax','FyPSmax','FyPFmax'}; removeBias =0;
 epochs={'SpeedAdapDiscont',  'TMafter','DelFAdapt'}; %Which should be the same as.. epochs={'SpeedAdapDiscont',  'DelFDeAdapt2Base','DelFAdapt'};
-RF=getForceResults(YoungSlopeData,params,groups,0, 0, 0, 1);
+RF=getForceResults(StudyData,params,groups,0, 0, 0, 1);
 conds = {'adaptation', 'TM post'};
-InclineDeclineFigure2V3(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups, YoungSlopeData, RF, epochs)
+InclineDeclineFigure2V3(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups, StudyData, RF, epochs)
 
-%Figure 2: Step Length Asymmetry
+%Figure 2 in the final paper
 params = {'netContributionNorm2'}; removeBias =1;
-RF_K=getForceResults(YoungSlopeData,params,groups,0, 0, 0, removeBias);
+RF_K=getForceResults(StudyData,params,groups,0, 0, 0, removeBias);
 epochs={'TMSteady', 'EarlyA', 'TMafter', 'DelFAdapt'};
 conds = {'TM base','adaptation','TM post'};
-InclineDeclineFigure3V3(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups, YoungSlopeData, RF_K, epochs)
+InclineDeclineFigure3V3(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups, StudyData, RF_K, epochs)
 
-%Figre 3: Step Length -- Panel A
-params = {'stepLengthSlow','stepLengthFast', 'stepLengthSlow','stepLengthFast'}; removeBias =0;
+%Figure 3 in the final paper
+params = {'stepLengthSlow','stepLengthFast'}; removeBias =0;
 conds = {'slow', 'fast', 'TM base','adaptation','TM post'};
-RF=getForceResults(YoungSlopeData,params,groups,0, 0, 0, removeBias);
-epochs={'EarlyA', 'DelFAdapt', 'TMSteady'};
-InclineDeclineFigure2(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups, YoungSlopeData, RF, epochs)
+RF=getForceResults(StudyData,params,groups,0, 0, 0, removeBias);
+RF_NoBias=getForceResults(StudyData,params,groups,0, 0, 0, 1);
+InclineDeclineFigure5V3(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups,  RF, RF_NoBias)
 
-%Figre 3: Step Length -- Panel B
-conds = {'slow', 'fast', 'TM base', 'adaptation','TM post'};
-params = {'stepLengthFast', 'stepLengthSlow'}; removeBias =0;
-RSL=getForceResults(YoungSlopeData,params,groups,0, 0, 0, removeBias);
-InclineDeclineFigure5(adaptDataList,params,conds,binWidth,trialMarkerFlag,indivSubFlag,IndivSubList,removeBias,groups, YoungSlopeData, RSL, [])
-
-%% Step Length Visualization
-%Figure 4, most panels
-StepLengthVis
-
-%%
-%Figure 6 and Figure 4, Panel D and F
-close all
-clear all
-clc
-load('YoungSlopeData.mat')
-groups = {'DeclineYoungAbrupt','FlatYoungAbrupt', 'InclineYoungAbrupt'};
+% Figure 6 in the final paper
 params = {'FyBSmax','FyBFmax','FyPSmax','FyPFmax', 'FyPmaxSym', 'FyBmaxSym', 'stepLengthFast', 'stepLengthSlow', 'netContributionNorm2', 'XSlow', 'XFast', 'alphaSlow', 'alphaFast'};%, 'SYZmag', 'FYZMag',  'FzFmax', 'FzSmax'};,  'FySBmaxQS','FySPmaxQS','FyFBmaxQS','FyFPmaxQS'
-RF=getForceResults(YoungSlopeData,params,groups,0, 0, 0, 1);
+RF=getForceResults(StudyData,params,groups,0, 0, 0, 1);
 params = {'spatialContributionNorm2','stepTimeContributionNorm2','velocityContributionNorm2','netContributionNorm2', 'stepLengthFast', 'stepLengthSlow'};
-RK=getResults(YoungSlopeData,params,groups,0, 0, 0, 1);
-InclineDeclineFigure6(YoungSlopeData,RF, RK, groups)
+RK=getResults(StudyData,params,groups,0, 0, 0, 1);
+InclineDeclineFigure6V2(StudyData,RF, RK, groups)
 
+%% LINEAR REGRESSION-- Analysis Not included in the paper
+InclineDeclineStudyMultipleLinearRegressionsV2
